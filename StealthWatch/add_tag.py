3386a03f3c886886ed3bb3a62774fc7bc08233b4
valor <valor312@gmail.com>
2019-10-26 09:00:06 -0200

    push

--- StealthWatch/add_tag.py
+++ StealthWatch/add_tag.py
@@ -1,39 +1,5 @@
 #!/usr/bin/env python
 
-"""
-This script will add a new tag (host group) in Stealthwatch using the REST API.
-
-For more information on this API, please visit:
-https://www.cisco.com/web/fw/stealthwatch/Online-Help/Content/Online-Help/enterprise-rest-api.htm
-
- -
-
-Script Dependencies:
-    requests
-Depencency Installation:
-    $ pip install requests
-
-System Requirements:
-    Stealthwatch Version: 7.0.0 or higher
-
-Copyright (c) 2019, Cisco Systems, Inc. All rights reserved.
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-"""
-
 import requests
 import json
 try:
