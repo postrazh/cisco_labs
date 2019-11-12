5a6376b9dae089bb1a5b51e399bfaf45bc419a5a
valor <valor312@gmail.com>
2019-11-13 00:05:32 +0800

    send second umbrella

--- Umbrella/Python/script2.py
+++ Umbrella/Python/script2.py
@@ -11,17 +11,16 @@ from datetime import datetime
 # umbrella investigate API KEY
 API_KEY = "0cad1811-f916-4976-8642-40add0e259c2"
 # csv file name
-CSV_FILE = 'investigate_domain.csv'
+CSV_FILE = 'risk_score_domain.csv'
 
 
-# Investigate URL
 # Sample domains:
 #   "internetbadguys.com"
 #   "stackoverflow.com"
 #   "cisco.com"
-def investigateDomain(domain):
+def getRiskScoreDomain(domain):
     # URL needed for the domain status and category
-    investigateUrl = "https://investigate.api.umbrella.com/domains/categorization/"
+    investigateUrl = "https://investigate.api.umbrella.com/domains/risk-score/"
 
     # create header for authentication
     headers = {
@@ -46,20 +45,8 @@ def investigateDomain(domain):
 
     # retrieve status for domain
     output = req.json()
-    domainOutput = output[domain]
-    domainStatus = domainOutput["status"]
-    # walk through different options of status
 
-    status = "Undefined"
-    if (domainStatus == -1):
-        status = "Malicious"
-    elif (domainStatus == 1):
-        status = "Clean"
-
-    security_categories = str(domainOutput["security_categories"])
-    content_categories = str(domainOutput["content_categories"])
-
-    return status, security_categories, content_categories
+    return output
 
 if __name__ == '__main__':
 
@@ -68,13 +55,17 @@ if __name__ == '__main__':
                  Umbrella Management
               ACME Inc, IT Security Department
 
-                Investigate a domain :""")
+                Risk scores for a domain :""")
 
     print("All output will be written to " + CSV_FILE + "\n")
 
     # Open the csv file
     with open(CSV_FILE, 'w') as csvFile:
         writer = csv.writer(csvFile)
+
+        header = ['Domain', 'Risk Score', 'Geo Popularity Score', 'Keyword Score', 'Lexical', 'Popularity 1 Day',
+                  'Popularity 30 Day', 'Popularity 7 Day', 'Popularity 90 Day', 'TLD Rank Score', 'Umbrella Block Status']
+        writer.writerow(header)
         # Loop forever
         while True:
             domain = input("  Enter a domain(Leave blank to end): ")
@@ -82,13 +73,25 @@ if __name__ == '__main__':
             if not domain:
                 sys.exit()
 
-            status, security_categories, content_categories = investigateDomain(domain)
+            output = getRiskScoreDomain(domain)
 
-            print("\tStatus = %s" % status)
-            print("\tSecurity Categories = %s" % security_categories)
-            print("\tContent Categories = %s" % content_categories)
+            riskScore = output["riskScore"]
+            features = output["features"]
+
+            print("\tRisk Scores = %s" % riskScore)
+            print("\tGeo Popularity Score = %s" % features[0]['normalizedScore'])
+            print("\tKeyword Score = %s" % features[1]['normalizedScore'])
+            print("\tLexical = %s" % features[2]['normalizedScore'])
+            print("\tPopularity 1 Day = %s" % features[3]['normalizedScore'])
+            print("\tPopularity 30 Day = %s" % features[4]['normalizedScore'])
+            print("\tPopularity 7 Day = %s" % features[5]['normalizedScore'])
+            print("\tPopularity 90 Day = %s" % features[6]['normalizedScore'])
+            print("\tTLD Rank Score = %s" % features[7]['normalizedScore'])
+            print("\tUmbrella Block Status = %s" % features[8]['normalizedScore'])
 
             # Save to a .csv file
-            row = [domain, status, security_categories, content_categories]
+            row = [domain, riskScore]
+            for item in features:
+                row.append(item['normalizedScore'])
             writer.writerow(row)
 
